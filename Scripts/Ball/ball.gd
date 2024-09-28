f6660aab1c920437c881737442945985022d0ed1
Pieter de Marez Oyens <oyenspieter@gmail.com>
2024-09-28 21:38:21 +0200

    changed the ball code and scene

--- Scripts/Ball/ball.gd
+++ Scripts/Ball/ball.gd
@@ -1,45 +1,58 @@
-extends CharacterBody2D
+extends Area2D
 
 signal player_collision
 
-@onready var sprite = $BallPivot/Sprite2D
+@onready var ball_sprite = $BallPivot/SprBall0
+@onready var arrow_sprite = $BallPivot/SprArrow1
 @onready var collision_shape = $CollisionShape2D
 
-@export var initial_velocity = 2
+@export var initial_speed = 100
+@export var sprite_offset_from_player = Vector2(35, 0)
+@export var arrow_sprite_offset_from_ball = Vector2(80, 0)
 
 var caught = false
-var direction = Vector2(5.0, -10.0).normalized()
+var ball_direction = Vector2(5.0, -10.0).normalized()
+var owned_by_player = null
 
 func _ready():
+	# Change the direction of a ball to a random direction on a circle
 	pass
 	
-func be_caught():
+func be_caught(catching_player):
 	caught = true
-	sprite.position = Vector2(100, 0)
 	collision_shape.disabled = true
 	
+	owned_by_player = catching_player
+	ball_sprite.position = sprite_offset_from_player
+	
+	arrow_sprite.visible = true
+	arrow_sprite.position = sprite_offset_from_player + arrow_sprite_offset_from_ball
 
 func be_thrown(new_direction):
-	#print(position, sprite.position)
+	
 	# Set the position of the Ball to the position of the sprite
-	position = sprite.global_position
+	position = ball_sprite.global_position
 	# Remove the offset
-	sprite.position = Vector2(0.0, 0.0)
-	#print(position, sprite.position)
+	ball_sprite.position = Vector2(0.0, 0.0)
+	ball_direction = new_direction
+	
 	caught = false
-	print(direction)
-	direction = new_direction
+	arrow_sprite.visible = false
 	collision_shape.disabled = false
 
 func _physics_process(delta):
 	if caught:
+		position = owned_by_player.position
 		look_at(get_global_mouse_position())
 	else: 
-		velocity = direction * initial_velocity
-		var collision = move_and_collide(velocity)
-		if collision:
-			velocity *= 0.0
-			player_collision.emit(collision.get_collider_id())	
+		position += ball_direction * initial_speed * delta
 			
-func move_with_player(player_position):
-	position = player_position
+func _on_body_entered(body):
+	if body.has_method("throw_ball"):
+		if true: # If a player catches the ball
+			be_caught(body)
+			player_collision.emit()
+		else:
+			print("Put the logic where a player dies")
+
+	
--- Scripts/Managers/ball_manager.gd
+++ Scripts/Managers/ball_manager.gd
@@ -7,14 +7,8 @@ func _on_player_ball_is_thrown(direction):
 	print("Ball is thrown, send instruction to ball")
 	ball.be_thrown(direction)
 
-
-func _on_ball_player_collision(collider_id):
-	#print("Collided with a Player1: " , collider_id)
-	#ball.position = player.position
-	ball.be_caught()
+func _on_ball_player_collision():
 	player.have_ball = true
 
-
-func _on_player_moving_with_ball(player_position):
-	print(player_position)
-	ball.move_with_player(player_position)
+#func _on_player_moving_with_ball(player_position):
+	#ball.move_with_player(player_position)
